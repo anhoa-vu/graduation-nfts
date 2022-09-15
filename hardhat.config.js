1b5782a97f0981aea82f57578ef4d397e9b17544
anhoa-vu <vah.2706@gmail.com>
2022-09-15 21:08:33 +0930

    add deploy task

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require("./scripts/deploy.js");
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.17",
+// };
+
+
 module.exports = {
   solidity: "0.8.17",
-};
+  defaultNetwork: "goerli",
+  networks: {
+   hardhat: {},
+   goerli: {
+     url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+     accounts: [process.env.ACCOUNT_PRIVATE_KEY]
+   }
+ }
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,14 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { task } = require("hardhat/config");
+const { getAccount } = require("./helpers");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+task("check-balance", "Prints out the balance of your account").setAction(async function (taskArguments, hre) {
+    const account = getAccount();
+    console.log(`Account balance for ${account.address}: ${await account.getBalance()}`);
+});
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+task("deploy", "Deploys the NFT.sol contract").setAction(async function (taskArguments, hre) {
+    const nftContractFactory = await hre.ethers.getContractFactory("NFT", getAccount());
+    const nft = await nftContractFactory.deploy();
+    console.log(`Contract deployed to address: ${nft.address}`);
 });
